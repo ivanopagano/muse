fe80669a4eeec55a63566092deace643aef4676a
Ivano Pagano <pagoda_5b@hotmail.com>
2013-07-17 02:45:14 +0200

    Include node4j deps. Add a WorldEngine actor.
    The new actor takes care of managing an inner graph of the world state.

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ name := "muse"
 
 version := "0.1-SNAPSHOT"
 
-libraryDependencies += "org.mashupbots.socko" %% "socko-webserver" % "0.3.0"
+libraryDependencies ++= Seq(
+	"org.mashupbots.socko" %% "socko-webserver" % "0.3.0",
+	"org.neo4j" % "neo4j" % "2.0.0-M03"
+)
---
+++ src/main/scala/it/pagoda5b/muse/WorldEngine.scala
@@ -0,0 +1,73 @@
+package it.pagoda5b.muse
+
+import akka.actor._
+
+class WorldEngine extends Actor {
+	import Player._
+
+	private val STORAGE_DIR = "/tmp/muse-neo4j-store"
+
+	private[this] val world = new WorldGraph(STORAGE_DIR)
+
+	def receive = {
+		case _ => 
+	}
+
+	override def postStop() {
+		world.stop()
+	}
+
+
+	private class WorldGraph(storeDir: String) {
+		import org.neo4j.graphdb._
+		import org.neo4j.graphdb.index._
+		import org.neo4j.graphdb.factory._
+		import WorldGraph._
+
+
+		require(storeDir != null)
+		private val graph: GraphDatabaseService = (new GraphDatabaseFactory).newEmbeddedDatabase(storeDir)
+		private val nodesIdx: Index[Node] = graph.index.forNodes("nodes")
+		private val relsIdx: RelationshipIndex = graph.index.forRelationships("rels")
+		private val startRoom: Long = populate(graph).getId
+
+		def stop(): Unit = graph.shutdown()
+
+	}
+
+	private object WorldGraph {
+		import org.neo4j.graphdb._
+
+		case object IS_IN extends RelationshipType {val name: String = "IS_IN"}
+		case object LEADS_TO extends RelationshipType {val name: String = "LEADS_TO"}
+
+		def populate(g: GraphDatabaseService): Node = {
+			def createRoom(name: String, desc: String): Node = {
+				val room = g.createNode
+				room.setProperty("name", name)
+				room.setProperty("description", desc)
+				room
+			}
+
+			def joinRooms(r1: Node, r2: Node, id: String, desc: String): Relationship = {
+				val exit = r1.createRelationshipTo(r2, LEADS_TO)
+				exit.setProperty("id", id)
+				exit.setProperty("description", desc)
+				exit
+			}
+
+			val courtyard = createRoom("cortile", "Un muro circonda questo piccolo spazio verde, costellato con un paio di alberi e molti cespugli")
+			val hall = createRoom("ingresso", "Una stanza confortevole e spaziosa, illuminata da un lampadario dall'aspetto antico e arredata decorosamente")
+			val terrace = createRoom("terrazza", "Da questa terrazza e' possibile intravedere in lontananza la linea del mare. Il pavimento e' composto di ceramiche dallo stile antico, ma niente di piu'")
+
+			joinRooms(courtyard, hall, "portone", "una massiccia porta che conduce all'edificio")
+			joinRooms(hall, courtyard, "uscita", "la porta verso l'esterno")
+
+			joinRooms(hall, terrace, "scalinata", "una scalinata in ebano lucido")
+			joinRooms(terrace, hall, "accesso", "una porta per la scalinata al piano inferiore")
+
+			courtyard
+		}
+
+	}
+}
